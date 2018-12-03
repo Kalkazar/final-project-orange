39fce8577191cb08766f66d83ad64fbfe398017e
John Estes <jestes93jobs@gmail.com>
2018-12-03 14:58:25 -0600

    Init Spring Boot

---
+++ src/main/java/com/cooksys/ftd/drivestorageorange/DriveStorageOrangeApplication.java
@@ -0,0 +1,12 @@
+package com.cooksys.ftd.drivestorageorange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriveStorageOrangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DriveStorageOrangeApplication.class, args);
+	}
+}
