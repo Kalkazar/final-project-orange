a62aeff22f46570ec7cedf45b0ffa5e4c252619c
Joshua Morris <jmorris252@students.sbts.edu>
2018-12-04 14:54:51 -0600

    Attempting to merge correctly

--- client/src/containers/App/index.js
+++ client/src/containers/App/index.js
@@ -7,39 +7,21 @@ import {
   TrashCard,
   FolderFunctionsCard
 } from '../../components/Card'
+import { Library } from '../Library'
+import { SideNav } from '../SideNav/SideNav'
 
 class App extends Component {
   render () {
     return (
-      <div className={styles.App}>
-        <FileCard
-          fileName={'PooFile.txt'}
-          fileId={1}
-          trashFile={console.log}
-          downloadFile={console.log}
-        />
-        <FolderCard
-          folderName={'PooFile.txt'}
-          folderId={1}
-          trashFolder={console.log}
-          downloadFolder={console.log}
-          openFolder={console.log}
-        />
-        <UploadCard
-          fileType='file'
-          upload={() => console.log('uploading file...')}
-        />
-        <TrashCard
-          name="Don't delete me.txt"
-          id={2}
-          deleteForever={console.log}
-          restore={console.log}
-          fileType='file'
-        />
-        <FolderFunctionsCard
-          createFolder={console.log}
-          uploadFolder={console.log}
-        />
+      <div className='container-fluid '>
+        <div className='row justify-content-center'>
+          <SideNav className=' col-sm-12 col-md-3 col-lg-2 col-xl-2'>
+            Navigation
+          </SideNav>
+          <Library className=' col-sm-12 col-md-9 col-lg-7 col-xl-6'>
+            Library
+          </Library>
+        </div>
       </div>
     )
   }
