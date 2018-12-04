1e7164a8cc6d14ae7093c3ac48670d9037b9e055
Joshua Morris <jmorris252@students.sbts.edu>
2018-12-04 14:42:48 -0600

    Added Icon to Inner Button Component

--- client/src/containers/App/index.js
+++ client/src/containers/App/index.js
@@ -3,17 +3,27 @@ import styles from './app.module.scss'
 import { FileCard } from '../../components/Card'
 import { SideNav } from '../SideNav/SideNav'
 import { Library } from '../Library'
+import InnerButton from '../../components/InnerButton'
+import { TrashIcon } from '../../components/Icon'
 
 class App extends Component {
   render () {
     return (
       <div className={`container-fluid ${styles.nopadding}`}>
         <div className={`row justify-content-center ${styles.nopadding}`}>
-          <div className=' col-sm-12 col-md-3 col-lg-2 col-xl-3'>
+          <div className=' col-sm-12 col-md-3 col-lg-3 col-xl-3'>
             <SideNav />
           </div>
           <div className=' col-sm-12 col-md-9 col-lg-8 col-xl-7'>
-            <Library />
+            <Library>
+              <FileCard
+                fileName={'PooFile.txt'}
+                fileId={1}
+                trashFile={console.log}
+                downloadFile={console.log}
+              />
+            </Library>
+            <InnerButton name={'Delete File'} icon={<TrashIcon />} />
           </div>
         </div>
       </div>
