79b1cd99f20a230d70b838a19e14d2c1c1491f2f
John Estes <jestes93jobs@gmail.com>
2018-12-04 16:42:12 -0600

    Implemented file DTO

--- server/src/main/java/com/cooksys/ftd/drivestorageorange/dtos/FileDTO.java
+++ server/src/main/java/com/cooksys/ftd/drivestorageorange/dtos/FileDTO.java
@@ -2,4 +2,47 @@ package com.cooksys.ftd.drivestorageorange.dtos;
 
 public class FileDTO {
 
+	private Long uid;
+
+	private String name;
+
+	private Long containerId;
+
+	private boolean inTrash;
+
+	public FileDTO() {
+	}
+
+	public Long getUid() {
+		return uid;
+	}
+
+	public void setUid(Long uid) {
+		this.uid = uid;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Long getContainerId() {
+		return containerId;
+	}
+
+	public void setContainerId(Long containerId) {
+		this.containerId = containerId;
+	}
+
+	public boolean isInTrash() {
+		return inTrash;
+	}
+
+	public void setInTrash(boolean inTrash) {
+		this.inTrash = inTrash;
+	}
+
 }
