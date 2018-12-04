cf0749dbea491b7990689b8369ee4a7b045ee62a
John Estes <jestes93jobs@gmail.com>
2018-12-04 16:14:59 -0600

    Implemented file mapper and helper mapping methods

--- server/src/main/java/com/cooksys/ftd/drivestorageorange/mappers/FileMapper.java
+++ server/src/main/java/com/cooksys/ftd/drivestorageorange/mappers/FileMapper.java
@@ -1,5 +1,28 @@
 package com.cooksys.ftd.drivestorageorange.mappers;
 
-public class FileMapper {
+import java.util.List;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+import com.cooksys.ftd.drivestorageorange.dtos.FileDTO;
+import com.cooksys.ftd.drivestorageorange.entities.FileEntity;
+import com.cooksys.ftd.drivestorageorange.mappers.helpers.FileMapperHelper;
+
+@Mapper(componentModel = "spring", uses = FileMapperHelper.class)
+public interface FileMapper {
+
+	public static FileMapper INSTANCE = Mappers.getMapper(FileMapper.class);
+
+	@Mapping(target = "containerId", source = "container")
+	FileDTO toDto(FileEntity entity);
+
+//	@Mapping(target = "containerId", source = "container")
+	List<FileDTO> toDto(List<FileEntity> entity);
+
+	FileEntity toEntity(FileDTO dto);
+
+	List<FileEntity> toEntity(List<FileDTO> entity);
 
 }
