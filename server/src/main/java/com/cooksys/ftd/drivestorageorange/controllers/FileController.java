a0851ad8325de86cd4b84f9dcecddde868582e23
John Estes <jestes93jobs@gmail.com>
2018-12-04 15:19:26 -0600

    Partially implemented classes

--- server/src/main/java/com/cooksys/ftd/drivestorageorange/controllers/FileController.java
+++ server/src/main/java/com/cooksys/ftd/drivestorageorange/controllers/FileController.java
@@ -1,90 +1,126 @@
 package com.cooksys.ftd.drivestorageorange.controllers;
 
-import java.util.List;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.cooksys.ftd.drivestorageorange.dtos.GetFilesDTO;
+import com.cooksys.ftd.drivestorageorange.entities.FileEntity;
+import com.cooksys.ftd.drivestorageorange.services.FileService;
 
 @RestController
 @RequestMapping("file")
 public class FileController {
-	
-//	@GetMapping("")
-//	public List<Hashtag> getAllTags() {
-//		return this.hashtagRepository.findAll();
-//	}
+
+	@Autowired
+	FileService fileService;
+
+	/**
+	 * Upload a new file
+	 * 
+	 * @return uid of newly uploaded file
+	 */
+//	@PostMapping("")
+//	public Map<String, Long> uploadFile(@RequestBody FileEntity uploadFile) {
+//		HashMap<String, Long> uploadUid = new HashMap<>();
 //
-//	@GetMapping("{label}")
-//	public Hashtag getHashtagByLabel(@PathVariable("label") String label) {
-//		return this.hashtagRepository.findByLabelCaseLess(label);
+//		uploadUid.put("uid", this.fileService.uploadFile(uploadFile));
+//		
+//		return uploadUid;
 //	}
-	
-	
-//	/file
-//	@POST /file/
-//	Upload a file
-//	Returns uid
 	
 	@PostMapping("")
-	public String uploadFile() {
-		return "";
-	}
-	
-//	@GET /file/{file_uid}
-//		Returns a file
-	
-	@GetMapping("{uid}")
-	public void getFile() {
+	public Map<String, Long> uploadFile(@RequestParam("file") MultipartFile uploadFile) {
+		HashMap<String, Long> uploadUid = new HashMap<>();
+
+		uploadUid.put("uid", this.fileService.uploadFile(uploadFile));
 		
+//		this.fileService.uploadFile(uploadFile);
+		
+		return uploadUid;
 	}
-	
+
+
+	/**
+	 * Returns a file via UID, if it exists
+	 * 
+	 * @return FileEntity
+	 * @throws IOException 
+	 */
+	@GetMapping("{uid}")
+	public FileEntity getFile(@PathVariable("uid") Long uid) throws IOException {
+//		FileEntity test = new FileEntity();
+//		File data = new File("example.txt");
+//		
+//
+//		test.setUid(new Long(23000));
+//		test.setName("testFile.txt");
+//		test.setCreated(new Date(new Long(12345)));
+//		test.setLastModified(new Date(new Long(12345)));
+//		test.setData(Files.readAllBytes(data.toPath()));
+//
+//		return test;
+
+		return this.fileService.getFileByUID(uid);
+	}
+
 //	@PATCH /file/{file_uid}/rename
 //		Renames the given file
-	
-//	@PatchMapping("{uid}/rename")
-//	public void renameFile() {
-//		
-//	}
-	
+
+	@PatchMapping("{uid}/rename/{newName}")
+	public void renameFile(@PathVariable("uid") Long uid, @PathVariable("newName") String newName) {
+		this.fileService.renameFile(uid, newName);
+	}
+
 //	@DELETE /file/{file_uid}/
 //		Moves a given file to the trash
-	
+
 	@DeleteMapping("{uid}")
-	public void deleteFile() {
-		
+	public void trashFile(@PathVariable("uid") Long uid) {
+		this.fileService.trashFile(uid);
 	}
-	
-	
+
+//	@PATCH /move-file/{file_uid}
+//	Moves a file to the root folder
+
+	@PatchMapping("move/{fileUid}")
+	public void moveFileToRoot(@PathVariable("fileUid") Long fileUid) {
+		this.fileService.moveFile(fileUid);
+	}
+
 //	@PATCH /move-file/{file_uid}/{folder_uid}
-//		Moves a file to the given folder. Moves 
-//		to root if {folder_uid} is not provided
-	
-	@PatchMapping("/move/{file_uid}/{folder_uid}")
-	public void moveFile() {
-		
+//		Moves a file to the given folder.
+
+	@PatchMapping("move/{fileUid}/{folderUid}")
+	public void moveFile(@PathVariable("fileUid") Long fileUid, @PathVariable("folderUid") Long folderUid) {
+		this.fileService.moveFile(fileUid, folderUid);
 	}
-	
-	
-//	@PATCH /move-file/{file_uid} // Pointless??!?!
-//		Moves a file to the root folder
-	
-	
+
 //	@POST /get-files/
 //		Params:
 //			(optional) sort_by: filename (default), uid
 //	(optional) page (default 1, 1-based indexing) 
 //	(optional) limit: 1-100 (default 100)
 //		Returns a list of up current file names and ids
-	
-	@PostMapping("get")
-	public void getFiles() {
-		
-	}
 
+	@PostMapping("get-files")
+	public void getFiles(@RequestBody GetFilesDTO options) {
+		this.fileService.getFiles(options.getSortBy(), options.getPage(), options.getLimit());
+	}
 
 }
--- server/src/main/java/com/cooksys/ftd/drivestorageorange/services/FileService.java
+++ server/src/main/java/com/cooksys/ftd/drivestorageorange/services/FileService.java
@@ -1,5 +1,139 @@
 package com.cooksys.ftd.drivestorageorange.services;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.cooksys.ftd.drivestorageorange.entities.FileEntity;
+import com.cooksys.ftd.drivestorageorange.repositories.FileRepository;
+
+@Service
 public class FileService {
+
+	@Autowired
+	FileRepository fileRepository;
+
+	/**
+	 * Uploads a file and returns it's UID <- Clean this up
+	 * @param uploadFile File to upload
+	 * @return Long
+	 */
+	public Long uploadFile(FileEntity uploadFile) {
+		return this.fileRepository.save(uploadFile).getUid();
+	}
+	
+	
+	public Long uploadFile(MultipartFile uploadFile) {
+		FileEntity newFile = new FileEntity();
+		
+		newFile.setName("test.txt");
+		
+		try {
+			newFile.setData(uploadFile.getBytes());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			newFile = null;
+		}
+		
+		if(newFile != null) {
+			return this.fileRepository.save(newFile).getUid();
+		} else {
+			return new Long(-1);
+		}
+	}
+
+	/**
+	 * Returns a file by UID
+	 * @param uid to search for
+	 * @return FileEntity
+	 */
+	public FileEntity getFileByUID(Long uid) {
+		return this.fileRepository.getOne(uid);
+	}
+
+	/**
+	 * Renames a file by UID
+	 * @param uid of file to rename
+	 * @param newName to assign to file
+	 */
+	public void renameFile(Long uid, String newName) {
+		FileEntity editingFile = this.getFileByUID(uid);
+		
+		editingFile.setName(newName);
+		
+		this.fileRepository.save(editingFile);
+	}
+
+	/**
+	 * [NOT FULLY IMPLEMENTED]
+	 * Stages a file for deletion "in trash" by UID
+	 * @param uid of file to put "in trash"
+	 */
+	public void trashFile(Long uid) {
+		FileEntity editingFile = this.getFileByUID(uid);
+		
+		editingFile.setInTrash(true);
+		
+		this.fileRepository.save(editingFile);
+		
+	}
+
+	/**
+	 * [NOT FULLY IMPLEMENTED]
+	 * Moves file into root directory by UID
+	 * @param fileUid of file to move
+	 */
+	public void moveFile(Long fileUid) {
+		FileEntity editingFile = this.getFileByUID(fileUid);
+		
+		// Requires FolderEntity implementation
+		editingFile.setContainer(null);
+		this.fileRepository.save(editingFile);
+	}
+
+	/**
+	 * [NOT IMPLEMENTED]
+	 * Moves file into specified directory by UID
+	 * @param fileUid of file to move
+	 * @param folderUid of destination to move file to
+	 */
+	public void moveFile(Long fileUid, Long folderUid) {
+		FileEntity editingFile = this.getFileByUID(fileUid);
+		
+		// Requires FolderEntity implementation
+	}
+
+	/**
+	 * [NOT IMPLEMENTED]
+	 * Returns all stored files via pagination
+	 * @param sortBy Method to sort files
+	 * @param page Page of sorted files to return
+	 * @param limit Number of files to return
+	 */
+	public void getFiles(String sortBy, Long page, Long limit) {
+//		this.fileRepository.findAll(sort)
+	}
+
+	/**
+	 * Permanently deletes file by UID
+	 * File must be inTrash to do so
+	 * @param uid to permanently delete
+	 */
+	public void deleteFile(Long uid) {
+		FileEntity deleteTarget = this.fileRepository.getOneTrashed(uid);
+		
+		if(deleteTarget != null) {
+			this.fileRepository.delete(deleteTarget);
+		} else {
+			System.out.println("No matching target for deletion!");
+		}
+	}
+
+	
 
 }
