6bbb38caa0b00baf721920267ccbce5f3a96233d
John Estes <jestes93jobs@gmail.com>
2018-12-06 09:56:51 -0600

    Added verbose comments, and props validation

--- client/src/containers/SideNav/SideNav.js
+++ client/src/containers/SideNav/SideNav.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux'
 import styles from './sideNav.module.scss'
 import { FolderFunctionsCard, UploadCard } from '../../components/Card'
 import NavButton from '../../components/NavButton'
+import PropTypes from 'prop-types'
 
 export class SideNav extends Component {
   render () {
@@ -11,29 +12,52 @@ export class SideNav extends Component {
         <NavButton
           text='Library'
           route='/library'
-          onClick={() => console.log('opening library')}
+          onClick={
+            // Placeholder - pls implement
+            () => console.log('opening library')
+          }
         />
         <NavButton
           text='Trash'
           route='/trash'
-          onClick={() => console.log('opening trash')}
+          onClick={
+            // Placeholder - pls implement
+            () => console.log('opening trash')
+          }
         />
         <FolderFunctionsCard
           createFolder={console.log}
-          uploadFolder={() => console.log('UPLOADING FOLDER')}
+          uploadFolder={
+            // Placeholder - pls implement
+            () => console.log('UPLOADING FOLDER')
+          }
         />
         <UploadCard
           fileType='file'
-          upload={() => console.log('UPLOADING FILE')}
+          upload={
+            // Placeholder - pls implement
+            () => console.log('UPLOADING FILE')
+          }
         />
       </div>
     )
   }
 }
 
+SideNav.propTypes = {
+  changeView: PropTypes.func,
+  uploadFolder: PropTypes.func,
+  uploadFile: PropTypes.func
+}
+
 const mapStateToProps = state => ({})
 
-const mapDispatchToProps = {}
+const mapDispatchToProps = dispatch => ({
+  // Hook up appropriate Redux methods
+  // changeView: view => dispatch(changeView(view)),
+  // uploadFolder: folder => dispatch(uploadFolder(folder)),
+  // uploadFile: file => dispatch(uploadFile(file))
+})
 
 export default connect(
   mapStateToProps,
