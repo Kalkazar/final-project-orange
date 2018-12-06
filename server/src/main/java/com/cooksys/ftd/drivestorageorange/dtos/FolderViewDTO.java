6bce9e6e995f90f03ff8148166c07b6ac38c7115
Forest Hill <forestwhill@gmail.com>
2018-12-06 13:32:34 -0600

    Added view DTO to send folder information without file data.

---
+++ server/src/main/java/com/cooksys/ftd/drivestorageorange/dtos/FolderViewDTO.java
@@ -0,0 +1,70 @@
+package com.cooksys.ftd.drivestorageorange.dtos;
+
+import java.util.Date;
+
+public class FolderViewDTO {
+
+	private Long uid;
+
+	private String name;
+
+	private Date created;
+
+	private Date lastModified;
+
+	private Long containerId;
+
+	private boolean inTrash;
+
+	public FolderViewDTO() {
+	}
+
+	public Long getUid() {
+		return uid;
+	}
+
+	public void setUid(Long uid) {
+		this.uid = uid;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Date getCreated() {
+		return created;
+	}
+
+	public void setCreated(Date created) {
+		this.created = created;
+	}
+
+	public Date getLastModified() {
+		return lastModified;
+	}
+
+	public void setLastModified(Date lastModified) {
+		this.lastModified = lastModified;
+	}
+
+	public Long getContainerId() {
+		return containerId;
+	}
+
+	public void setContainerId(Long containerId) {
+		this.containerId = containerId;
+	}
+
+	public boolean isInTrash() {
+		return inTrash;
+	}
+
+	public void setInTrash(boolean inTrash) {
+		this.inTrash = inTrash;
+	}
+
+}
