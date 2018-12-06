fe3384ff65756b7cf254948714830faeacb836e6
Forest Hill <forestwhill@gmail.com>
2018-12-06 09:14:57 -0600

    Reduced unnecessary complexity of file mapper and controller.

--- server/src/main/java/com/cooksys/ftd/drivestorageorange/mappers/FileMapper.java
+++ server/src/main/java/com/cooksys/ftd/drivestorageorange/mappers/FileMapper.java
@@ -4,25 +4,16 @@ import java.util.List;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.factory.Mappers;
 
 import com.cooksys.ftd.drivestorageorange.dtos.FileDTO;
 import com.cooksys.ftd.drivestorageorange.entities.FileEntity;
-import com.cooksys.ftd.drivestorageorange.mappers.helpers.FileMapperHelper;
 
-@Mapper(componentModel = "spring", uses = FileMapperHelper.class)
+@Mapper(componentModel = "spring")
 public interface FileMapper {
 
-	public static FileMapper INSTANCE = Mappers.getMapper(FileMapper.class);
-
-	@Mapping(target = "containerId", source = "container")
+	@Mapping(source = "container.uid", target = "containerId")
 	FileDTO toDto(FileEntity entity);
 
-//	@Mapping(target = "containerId", source = "container")
 	List<FileDTO> toDto(List<FileEntity> entity);
-
-	// Unused methods
-//	FileEntity toEntity(FileDTO dto);
-//	List<FileEntity> toEntity(List<FileDTO> entity);
 
 }
