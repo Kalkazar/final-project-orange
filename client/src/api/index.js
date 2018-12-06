cbee0e3a2bfba1fa57b2851860a23cd19106e91f
John Estes <jestes93jobs@gmail.com>
2018-12-06 10:15:01 -0600

    Updated to ES6 standard, and added comments

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,12 +1,20 @@
 import { files, folders, file } from '../helpers/mock-responses'
-const axios = require('axios')
+import Axios from 'axios'
 
 export const getFileList = () =>
   new Promise((resolve, reject) => {
     resolve(files)
   })
 
+// Appropriate endpoint - get all files
+// Axios.get('file/all')
+//   .then()
+
 export const getFolderList = () =>
   new Promise((resolve, reject) => {
     resolve(folders)
   })
+
+// Appropriate endpoint - get all folders
+// Axios.get('folder/all')
+//   .then()
