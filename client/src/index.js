2b58d11ed4acbd5e84f961641c52cd347883f9a5
John Estes <jestes93jobs@gmail.com>
2018-12-08 23:36:02 -0600

    Refactored and simplified

--- client/src/index.js
+++ client/src/index.js
@@ -5,12 +5,13 @@ import App from './containers/App'
 import { Provider } from 'react-redux'
 import createHistory from 'history/createBrowserHistory'
 import { BrowserRouter as Router } from 'react-router-dom'
-import getStore, { Library } from './ducks'
+import getStore, { Library, UI } from './ducks'
 
 const { getFiles } = Library
 const history = createHistory()
 const store = getStore({}, history)
-store.dispatch(getFiles())
+// store.dispatch(getFiles())
+store.dispatch(UI.initData())
 window.store = store
 setTimeout(() => console.log(store.getState()), 500) // Why?
 
