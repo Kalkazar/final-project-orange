2dbde872f1f4c514fce1032a2aed470b9abfd3ae
John Estes <jestes93jobs@gmail.com>
2018-12-10 10:53:01 -0600

    Added Paginator to Library view

--- client/src/containers/Library/index.js
+++ client/src/containers/Library/index.js
@@ -1,47 +1,51 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 import styles from './library.module.scss'
-import {
-  FileCard,
-  FolderCard
-} from '../../components/Card'
+import { FileCard, FolderCard } from '../../components/Card'
+import Pagination from '../../components/Pagination'
 import { Library as LibraryDuck, Modals as ModalsDuck } from '../../ducks'
 import { LiveEndpoints } from '../../api'
 
-const { trashFile, trashFolder } = LibraryDuck
+const { trashFile, trashFolder, setPage } = LibraryDuck
 const { toggleOpenFolder, openFolder, editFile } = ModalsDuck
 
 class Library extends Component {
   render () {
     return (
-      <div className={styles.libDiv}>
-        <span className={styles.pathSpan}>this/is/the/path/span</span>
-        {/* Checking props manually for testing */}
-        {console.log('Library props', this.props)}
+      <Fragment>
+        <div className={styles.libDiv}>
+          <span className={styles.pathSpan}>this/is/the/path/span</span>
+          {/* Checking props manually for testing */}
+          {console.log('Library props', this.props)}
 
-        {/* If props.activePage exists, render cards for items */}
-        {this.props.activePage ? this.props.activePage.map((e, i) => {
-          return e.isFolder
-            ? (<FolderCard
-              key={i}
-              folderName={e.name}
-              folderId={e.uid}
-              openFolder={() => this.props.openFolder(e)}
-              trashFolder={() => this.props.trashFolder(e.uid)}
-              downloadFolder={() => LiveEndpoints.Folder.downloadFolder(e.uid)}
-            />)
-            : (<FileCard
-              key={i}
-              fileName={e.name}
-              fileId={e.uid}
-              moveFile={() => this.props.editFile(e)}
-              trashFile={() => this.props.trashFile(e.uid)}
-              downloadFile={() => LiveEndpoints.File.downloadFile(e.uid)}
-            />)
-        }
-        ) : null}
-      </div>
+          {this.props.activePage ? this.props.activePage.map((e, i) => {
+            return e.isFolder
+              ? (<FolderCard
+                key={i}
+                folderName={e.name}
+                folderId={e.uid}
+                openFolder={() => this.props.openFolder(e)}
+                trashFolder={() => this.props.trashFolder(e.uid)}
+                downloadFolder={() => LiveEndpoints.Folder.downloadFolder(e.uid)}
+              />)
+              : (<FileCard
+                key={i}
+                fileName={e.name}
+                fileId={e.uid}
+                moveFile={() => this.props.editFile(e)}
+                trashFile={() => this.props.trashFile(e.uid)}
+                downloadFile={() => LiveEndpoints.File.downloadFile(e.uid)}
+              />)
+          }
+          ) : null}
+        </div>
+        <Pagination
+          currentPage={this.props.currentPage + 1}
+          totalPages={this.props.totalPages}
+          setPage={this.props.setPage}
+        />
+      </Fragment>
     )
   }
 }
@@ -52,22 +56,25 @@ Library.propTypes = {
   trashFolder: PropTypes.func,
   toggleOpenFolder: PropTypes.func,
   openFolder: PropTypes.func,
-  editFile: PropTypes.func
+  editFile: PropTypes.func,
+  currentPage: PropTypes.number,
+  totalPages: PropTypes.number,
+  setPage: PropTypes.func
 }
 
 const mapStateToProps = state => ({
-  activePage: state.library.currentList
+  activePage: state.library.currentList,
+  currentPage: state.trash.currentPage,
+  totalPages: state.trash.totalPages
 })
 
 const mapDispatchToProps = dispatch => ({
-  // Hook up appropriate Redux methods
   trashFile: uid => dispatch(trashFile(uid)),
   trashFolder: uid => dispatch(trashFolder(uid)),
   toggleOpenFolder: () => dispatch(toggleOpenFolder()),
   openFolder: folder => dispatch(openFolder(folder)),
-  editFile: file => dispatch(editFile(file))
-  // uploadFolder: folder => dispatch(uploadFolder(folder)),
-  // uploadFile: file => dispatch(uploadFile(file))
+  editFile: file => dispatch(editFile(file)),
+  setPage: index => dispatch(setPage(index))
 })
 
 export default connect(
