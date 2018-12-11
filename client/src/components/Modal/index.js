547a74a01ac46d1f0fd0e161dd322522feacf92e
John Estes <jestes93jobs@gmail.com>
2018-12-10 22:56:36 -0600

    Created generic component for verification modal, and implemented in primary Modal component

--- client/src/components/Modal/index.js
+++ client/src/components/Modal/index.js
@@ -14,14 +14,16 @@ import {
   ListGroup,
   ListGroupItem
 } from 'reactstrap'
-// import { LiveEndpoints } from '../../api'
+import VerifyModal from './VerifyModal'
 import { Modals as ModalsDuck, Library as LibraryDuck } from '../../ducks'
 
 const { 
   hideCreateFolderAction, 
   toggleCreateFolder, 
   toggleEditFile, 
-  toggleOpenFolder
+  toggleOpenFolder,
+  toggleVerify,
+  validVerificationMethods: VVM
 } = ModalsDuck
 
 const { createNewFolder, renameFile, moveFile } = LibraryDuck
@@ -61,7 +63,6 @@ class Modals extends Component {
   render () {
     return (
       <Fragment>
-
         {/* Create Folder Modal */}
         <Modal isOpen={this.props.showCreateFolder} toggle={this.props.toggleCreateFolder} className={this.props.className}>
           <ModalHeader toggle={this.props.toggleCreateFolder}>Create a New Folder</ModalHeader>
@@ -98,7 +99,6 @@ class Modals extends Component {
           </ModalBody>
           <ModalFooter>
             <Button color="secondary" onClick={this.props.toggleOpenFolder}>Close</Button>{' '}
-            {/* <Button color="primary" onClick={this.openFolderHandleClick}>Click Action</Button> */}
           </ModalFooter>
         </Modal>
 
@@ -128,6 +128,52 @@ class Modals extends Component {
           </ModalFooter>
         </Modal>
 
+        {/* Verification Modal */}
+        <VerifyModal 
+            isOpen={this.props.showVerify}
+            toggleMethod={this.props.toggleVerify}
+            headerText={
+              (() => {
+                switch (this.props.verifyMethod) {
+                  case VVM.RESTORE_ALL: return 'Are you sure?'
+                  case VVM.DELETE_ALL: return 'Are you sure?'
+                  default: return 'Are you sure?'
+                }
+              })()
+            }
+            bodyText={
+              (() => {
+                switch (this.props.verifyMethod) {
+                  case VVM.RESTORE_ALL: return 'This operation will restore all ' +
+                      'items from the trashbin to the library. ' +
+                      'Are you sure you want to do this?'
+                  case VVM.DELETE_ALL: return 'This operation will permanently, ' +
+                      'irreversibly remove all items within the trashbin. ' +
+                      'Are you sure you want to do this?'
+                  default: return 'This message is only displayed ' +
+                      'when a verification method has not been set. ' +
+                      'Hopefully this was intended. Good luck :)'
+                }
+              })()
+            }
+            confirmMethod={
+              (() => {
+                switch (this.props.verifyMethod) {
+                  case VVM.RESTORE_ALL: return () => console.log('Fired restoreAll confirmMethod!')
+                  case VVM.DELETE_ALL: return () => console.log('Fired deleteAll confirmMethod!')
+                  default: return () => console.log('Fired default confirmMethod!')
+                }
+              })()
+            }
+            declineMethod={
+              (() => {
+                switch (this.props.verifyMethod) {
+                  case VVM.RESTORE_ALL: return () => this.props.toggleVerify()
+                  case VVM.DELETE_ALL: return () => this.props.toggleVerify()
+                  default: return () => this.props.toggleVerify()
+                }
+              })()
+            } />
       </Fragment>
     )
   }
@@ -137,10 +183,16 @@ Modals.propTypes = {
   showCreateFolder: PropTypes.bool,
   showOpenFolder: PropTypes.bool,
   showEditFile: PropTypes.bool,
+  showVerify: PropTypes.bool,
+
   hideCreateFolder: PropTypes.func,
+
   toggleCreateFolder: PropTypes.func,
   toggleEditFile: PropTypes.func,
   toggleOpenFolder: PropTypes.func,
+  toggleVerify: PropTypes.func,
+  
+  verifyMethod: PropTypes.string,
   createNewFolder: PropTypes.func,
   targetFolder: PropTypes.object,
   targetFile: PropTypes.object,
@@ -150,10 +202,13 @@ Modals.propTypes = {
 }
 
 const mapStateToProps = state => ({
-  // totalPages: state.library.totalPages,
   showCreateFolder: state.modals.createFolder.show,
   showOpenFolder: state.modals.openFolder.show,
   showEditFile: state.modals.editFile.show,
+  showVerify: state.modals.verify.show,
+
+  verifyMethod: state.modals.verify.method,
+
   targetFolder: state.modals.openFolder.targetFolder,
   targetFile: state.modals.editFile.targetFile,
   folderList: state.library.folderList
@@ -164,12 +219,11 @@ const mapDispatchToProps = dispatch => ({
   toggleCreateFolder: () => dispatch(toggleCreateFolder()),
   toggleEditFile: () => dispatch(toggleEditFile()),
   toggleOpenFolder: () => dispatch(toggleOpenFolder()),
+  toggleVerify: () => dispatch(toggleVerify()),
+
   createNewFolder: folderName => dispatch(createNewFolder(folderName)),
   renameFile: (uid, newName) => dispatch(renameFile(uid, newName)),
   moveFile: (uid, folderUid) => dispatch(moveFile(uid, folderUid))
-
-  // setPage: page => dispatch(setPage(page)),
-  // getFiles: () => dispatch(getFiles())
 })
 
 export default connect(
