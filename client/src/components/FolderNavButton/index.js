616f344bc54cedd96b1fd1cdd29286a230b00a66
John Estes <jestes93jobs@gmail.com>
2018-12-10 20:44:47 -0600

    Fixed important props

--- client/src/components/FolderNavButton/index.js
+++ client/src/components/FolderNavButton/index.js
@@ -9,7 +9,7 @@ import styles from './folderNavButton.module.scss'
 const FolderNavButton = props => (
   <div className={`${styles.folderNavButtonClass}`} onClick={props.onClick}>
     <div className={`${styles.buttonContent}`}>
-      <span>Create Folder</span>
+      <span>{props.text}</span>
       <div className={`${styles.buttonIcon}`}>{props.icon}</div>
     </div>
   </div>
